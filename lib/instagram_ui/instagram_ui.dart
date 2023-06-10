e7ff219301d4b217717f79d87ee2d93943f0c4e9
Efren Ruiz <Efren.BR2E@gmail.com>
2023-06-10 23:34:40 +0200

    Iniciando proyecto

---
+++ lib/instagram_ui/instagram_ui.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InstagramUI extends StatelessWidget {
+  const InstagramUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
